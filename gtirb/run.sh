a31d1c0a6bc4180b8a78b45a1919a2485e2831ab
agle <a.michael1@uq.edu.au>
2026-05-29 14:48:12 +1000

    test

--- gtirb/run.sh
+++ gtirb/run.sh
@@ -1,8 +1,11 @@
-
+#!/usr/bin/env bash
 
 for i in $(find -iname *.gts) ; do 
+  mkdir -p $(dirname $i)
+  oname="out/$i-initial-gts.expected"
+  mkdir -p $(dirname $oname)
   cat << EOF | dune exec bincaml script -
   (load-gtirb $i)
-  (dump-il)
+  (dump-il $oname)
 EOF
 done
