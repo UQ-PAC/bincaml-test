02b55b556d1d45a028b04bcd26aea0277867d81b
agle <a.michael1@uq.edu.au>
2026-06-12 12:53:00 +1000

    generate diff rather than using folder diff

---
+++ gtirb/dune.sh
@@ -0,0 +1,30 @@
+
+#!/usr/bin/env bash
+
+
+cat << EOF
+(rule
+ (alias runtest)
+ (deps
+  %{bin:bincaml}
+  (source_tree basil)
+  (source_tree expect)
+  run.sh)
+ (action
+  (no-infer
+   (progn
+    (bash pwd)
+    (system ./run.sh)
+EOF
+
+echo "    (concurrent"
+for i in $(find . -iname '*.gts') ; do
+  oname="$i-initial-gts.expected"
+  mkdir -p $(dirname $oname)
+  echo "      (diff \"expect/$oname\" \"out/$oname\")"
+done
+echo "    )"
+
+cat << EOF
+))))
+EOF
